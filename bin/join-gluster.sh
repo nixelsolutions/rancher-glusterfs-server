c09faee5b3bdd83c402163e08b93700483cc4aa8
Manel Martinez Gonzalez <manel@MacBook-de-Manel.local>
2015-06-09 15:52:23 +0200

    first commit

--- bin/join-gluster.sh
+++ bin/join-gluster.sh
@@ -44,7 +44,7 @@ fi
 # If PEER has requested me to join him, just wait for a while
 SEMAPHORE_FILE=/tmp/adding-gluster-node.${PEER}
 if [ -e ${SEMAPHORE_FILE} ]; then
-   echo "=> Seems like peer ${PEER} has just requested me to join him to the cluster"
+   echo "=> Seems like peer ${PEER} has just requested me to join him"
    echo "=> So I'm waiting for 20 seconds to finish it..."
    sleep 20
 fi
