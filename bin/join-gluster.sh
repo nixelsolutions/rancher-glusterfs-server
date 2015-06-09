082ed3f953d2238f2607d71a086bd8ff31cbce77
Manel Martinez Gonzalez <manel@MacBook-de-Manel.local>
2015-06-09 16:05:21 +0200

    first commit

--- bin/join-gluster.sh
+++ bin/join-gluster.sh
@@ -46,7 +46,7 @@ SEMAPHORE_FILE=/tmp/adding-gluster-node.${PEER}
 if [ -e ${SEMAPHORE_FILE} ]; then
    echo "=> Seems like peer ${PEER} has just requested me to join him"
    echo "=> So I'm waiting for 20 seconds to finish it..."
-   sleep 20
+   sleep 60
 fi
 check_if_already_joined
 
