b25b099e25393730181fe74421d9aca78d440d67
root <root@ip-172-31-34-74.us-west-2.compute.internal>
2015-08-08 17:32:54 +0000

    Added quota support

--- bin/manage-gluster-quota.sh
+++ bin/manage-gluster-quota.sh
@@ -13,8 +13,8 @@ source ${GLUSTER_CONF_FLAG}
 
 [ "$DEBUG" == "1" ] && set -x && set +e
 
-function echo() {
-   builtin echo $(basename $0): [From container ${MY_RANCHER_IP}] $1
+function echo_msg() {
+   echo $(basename $0): [From container ${MY_RANCHER_IP}] $1
 }
 
 # Params:
@@ -36,12 +36,12 @@ while getopts ":o:d:q" PARAMS; do
       esac
 done
 
-[ -z "$OPERATION" ] && echo "Error, operation parameter is missing (parameter -o)" && exit $EXIT_ERROR
-[ -z "$DIRECTORY" ] && echo "Error, directory parameter is missing (parameter -d)" && exit $EXIT_ERROR
+[ -z "$OPERATION" ] && echo_msg "Error, operation parameter is missing (parameter -o)" && exit $EXIT_ERROR
+[ -z "$DIRECTORY" ] && echo_msg "Error, directory parameter is missing (parameter -d)" && exit $EXIT_ERROR
 
 case $OPERATION in
 SET)
-   [ -z "$QUOTA" ] && echo "Error, quota arameter is missing (parameter -q)" && exit $EXIT_ERROR
+   [ -z "$QUOTA" ] && echo_msg "Error, quota arameter is missing (parameter -q)" && exit $EXIT_ERROR
 
    # Set quota on directory
    msg=`gluster volume quota ${GLUSTER_VOL} limit-usage /${DIRECTORY} $QUOTA`
@@ -57,15 +57,15 @@ SHOW)
    msg=`gluster volume quota ${GLUSTER_VOL} list /${DIRECTORY}` 
    if [ $? -eq 0 ]; then
       msg=`echo $msg | grep "^/${DIRECTORY}"`
-      echo "SUCCESS: $msg"
+      echo_msg "SUCCESS: $msg"
       exit $EXIT_OK
    else
-      echo "ERROR: $msg"
+      echo_msg "ERROR: $msg"
       exit $EXIT_ERROR
    fi
 ;;
 *)
-   echo "ERROR: unknown operation $OPERATION"
+   echo_msg "ERROR: unknown operation $OPERATION"
    exit $EXIT_ERROR
 ;;
 esac
